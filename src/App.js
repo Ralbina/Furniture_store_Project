83b63f03c6745e78afdc4727e6b9bae048ea988c
Ralbina <albinarhm93@gmail.com>
2022-06-15 16:49:00 +0600

    comit design container3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import ProductContextProvider from "./Components/Context/ProductContext";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
 import MainRoutes from "./MainRoutes";
 
 function App() {
   return (
     <BrowserRouter>
       <ProductContextProvider>
+        <Navbar />
         <MainRoutes />
       </ProductContextProvider>
     </BrowserRouter>
--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AddProduct from "./Components/Admin/AddProduct";
 import EditProduct from "./Components/Admin/EditProduct";
+import Home from "./Components/Home/Home";
 import ProductDetails from "./Components/ProductDetails/ProductDetails";
 import ProductList from "./Components/ProductList/ProductList";
 
 const MainRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/add" element={<AddProduct />} />
       <Route path="/edit/:id" element={<EditProduct />} />
       <Route path="/list" element={<ProductList />} />
